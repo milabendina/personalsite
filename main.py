b2a3749efc6e1438d224b1118e855851f6fe13ca
Mila <65489941+milabendina@users.noreply.github.com>
2021-05-01 20:58:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
+@app.route('/index')
 def home():
-	return render_template('home.html')
+	return render_template('index.html')
 
 app.run(host='0.0.0.0', port=8080)
